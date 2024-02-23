43d83cfe56913579c5cc8768185fe739c6cae043
omarhuanca <oma378501@yahoo.com>
2024-02-23 18:12:12 -0400

    add basic test on application.

--- tests/test_scrape_facebook.py
+++ tests/test_scrape_facebook.py
@@ -15,6 +15,20 @@ class XTest(unittest.TestCase):
         configObj = x.read_config_path()
         self.assertNotEqual('', configObj.get('credentials', 'password'))
 
+    def test03(self):
+        x = X("config.txt")
+        identifier = x.get_profile_from_url("https://www.facebook.com/profile.php?id=61556176318834")
+        self.assertEqual("61556176318834", identifier)
+
+    def test04(self):
+        x = X("config.txt")
+        identifier = x.get_profile_from_url("https://www.facebook.com/vanesa.morales.3994885")
+        self.assertEqual("vanesa.morales.3994885", identifier)
+
+    def test05(self):
+        x = X("config.txt")
+        identifier = x.get_profile_from_url("https://www.facebook.com/omar.huancabalboa")
+        self.assertEqual("omar.huancabalboa", identifier)
 
 if __name__ == "__main__":
     unittest.main()
